ce19779e3f523d12073d89b99a13e7d4491386c2
Sergey <yougodxaso@gmail.com>
2020-10-11 22:45:38 +0300

    Добавлен компонент для отправки комментариев

---
+++ components/main/CommentForm.vue
@@ -0,0 +1,67 @@
+<template>
+  <el-form
+    :model="controls"
+    :rules="rules"
+    ref="form"
+    @submit.native.prevent="onSubmit"
+  >
+    <h1>Добавить комментарий</h1>
+    <el-form-item label="Ваше имя" prop="name">
+      <el-input v-model.trim="controls.name" />
+    </el-form-item>
+    <el-form-item label="Текст комментария" prop="text">
+      <el-input
+        type="textarea"
+        v-model.trim="controls.text"
+        resize="none"
+        :rows="2"
+      />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" native-type="submit" round>
+        Добавить комментарий
+      </el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      controls: {
+        name: "",
+        text: "",
+      },
+      rules: {
+        name: [
+          {
+            required: true,
+            message: "Имя не должно быть пустым",
+            trigger: "blur",
+          },
+        ],
+        text: [
+          {
+            required: true,
+            message: "Введите ваш комментарий",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          console.log("Form is Valid");
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
