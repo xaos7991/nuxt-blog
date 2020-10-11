321cc5d87803d4b783208da44a271398756c6098
Sergey <yougodxaso@gmail.com>
2020-10-11 19:35:27 +0300

    Создал страницу 404

---
+++ layouts/error.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="error-layout">
+    <div class="error-content">
+      <h1 class="error-title">Ошибка 404</h1>
+      <nuxt-link to="/">Вернуться на главную</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.error-layout {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.error-title {
+  color: rgb(145, 6, 6);
+  margin-bottom: 1rem;
+}
+
+.error-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 4rem;
+}
+</style>
