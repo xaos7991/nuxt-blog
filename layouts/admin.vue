634bffec318cff16524aacace4b408f4c375a299
Sergey <yougodxaso@gmail.com>
2020-10-14 20:31:37 +0300

    добавил layout для админской части

---
+++ layouts/admin.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="admin-layot-wrap">
+    <el-container :style="{ height: '100%' }">
+      <el-aside width="250px">
+        <AppAside />
+      </el-aside>
+      <el-main>
+        <Nuxt />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import AppAside from "@/components/admin/Aside";
+export default {
+  components: {
+    AppAside,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.admin-layout-wrap {
+  width: 100%;
+  height: 100vh;
+}
+</style>
