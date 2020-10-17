17e92ab6940e23f315004ace0dcdb10a81a4b17c
Sergey <yougodxaso@gmail.com>
2020-10-17 17:46:06 +0300

    Добавил новый layout

---
+++ layouts/empty.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="empty-layout">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.empty-layout {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding-top: 4rem;
+}
+</style>
