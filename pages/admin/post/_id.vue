a19f4fd6e047332f4b69582aee3cbb84dfa7964a
Sergey <yougodxaso@gmail.com>
2020-10-18 18:23:25 +0300

    Добавил странице поста в админке

---
+++ pages/admin/post/_id.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item to="/admin/list">Посты</el-breadcrumb-item>
+      <el-breadcrumb-item>
+        {{ $route.params.id }}
+      </el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "admin",
+  middleware: ["admin-auth"],
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
