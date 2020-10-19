2df1e1335d07f141795faf8f15b0cba4c61f4c35
Sergey <yougodxaso@gmail.com>
2020-10-19 23:53:34 +0300

    Прокинул новые методы

--- store/post.js
+++ store/post.js
@@ -1,15 +1,34 @@
+const posts = [
+    { title: 'Post1', date: new Date(), views: 22, comments: [1, 2], _id: 'id1' },
+    { title: 'Post2', date: new Date(), views: 22, comments: [1, 2], _id: 'id2' }
+]
+
 export const actions = {
     async fetchAdmin({ }) {
         return await new Promise(resolve => {
             setTimeout(() => {
-                resolve([
-                    { title: 'Post1', date: new Date(), views: 22, comments: [1, 2], _id: Math.random() },
-                    { title: 'Post2', date: new Date(), views: 22, comments: [1, 2], _id: Math.random() }
-                ])
+                resolve(posts)
             }, 1000);
         })
     },
     async remove({ }, id) {
 
+    },
+    async update({ }, { id, text }) {
+
+    },
+    async create({ }, { title, text }) {
+        return await new Promise(resolve => {
+            setTimeout(() => {
+                resolve()
+            }, 1000);
+        })
+    },
+    async fetchAdminById({ }, id) {
+        return await new Promise(resolve => {
+            setTimeout(() => {
+                resolve(posts.find(p => p._id === id))
+            }, 1000);
+        })
     }
 }
