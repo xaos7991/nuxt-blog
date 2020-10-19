738755494b78d4670e97d6c86899f4bf45b8867a
Sergey <yougodxaso@gmail.com>
2020-10-19 23:53:46 +0300

    Добавил вёрстку

--- pages/admin/create.vue
+++ pages/admin/create.vue
@@ -1,13 +1,88 @@
 <template>
-  <div>create</div>
+  <el-form
+    :model="controls"
+    :rules="rules"
+    ref="form"
+    @submit.native.prevent="onSubmit"
+  >
+    <h1 class="mb">Создать новый пост</h1>
+    <el-form-item label="Введите название поста" prop="title">
+      <el-input v-model.trim="controls.title" />
+    </el-form-item>
+    <el-form-item label="Текст в формате .md или .html" prop="text">
+      <el-input
+        type="textarea"
+        v-model.trim="controls.text"
+        resize="none"
+        :rows="10"
+      />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" native-type="submit" round :loading="loading">
+        Создать пост
+      </el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
 export default {
   layout: "admin",
   middleware: ["admin-auth"],
+  data() {
+    return {
+      loading: false,
+      controls: {
+        title: "",
+        text: "",
+      },
+      rules: {
+        text: [
+          {
+            required: true,
+            message: "Текст не должен быть пустым",
+            trigger: "blur",
+          },
+        ],
+        title: [
+          {
+            required: true,
+            message: "Название поста не может быть пустым",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.form.validate(async (valid) => {
+        if (valid) {
+          this.loading = true;
+
+          const formData = {
+            title: this.controls.title,
+            text: this.controls.text,
+          };
+
+          try {
+            await this.$store.dispatch("post/create", formData);
+            this.controls.text = "";
+            this.controls.title = "";
+            this.$message.success("Пост создан");
+          } catch (e) {
+          } finally {
+            this.loading = false;
+          }
+        }
+      });
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+form {
+  width: 600px;
+}
 </style>
