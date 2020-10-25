b1f9998c4d42f2d6a04f4b726839d41ca4d06af9
Sergey <yougodxaso@gmail.com>
2020-10-25 23:59:03 +0300

    Добавил uploader

--- pages/admin/create.vue
+++ pages/admin/create.vue
@@ -27,6 +27,19 @@
         </VueMarkdown>
       </div>
     </el-dialog>
+    <el-upload
+      class="mb"
+      drag
+      action="https://jsonplaceholder.typicode.com/posts/"
+      :on-change="handleImageChange"
+      :auto-upload="false"
+    >
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">
+        Перетащите картинку <em>или нажмите</em>
+      </div>
+      <div class="el-upload__tip" slot="tip">Файлы с расширением jpg/png</div>
+    </el-upload>
     <el-form-item>
       <el-button type="primary" native-type="submit" round :loading="loading">
         Создать пост
@@ -41,6 +54,7 @@ export default {
   middleware: ["admin-auth"],
   data() {
     return {
+      image: null,
       previewDialog: false,
       loading: false,
       controls: {
@@ -66,25 +80,32 @@ export default {
     };
   },
   methods: {
+    handleImageChange(file, fileList) {
+      this.image = file.raw;
+    },
     onSubmit() {
       this.$refs.form.validate(async (valid) => {
-        if (valid) {
+        if (valid && this.image) {
           this.loading = true;
 
           const formData = {
             title: this.controls.title,
             text: this.controls.text,
+            image: this.image,
           };
 
           try {
             await this.$store.dispatch("post/create", formData);
             this.controls.text = "";
             this.controls.title = "";
+            this.image = null;
             this.$message.success("Пост создан");
           } catch (e) {
           } finally {
             this.loading = false;
           }
+        } else {
+          this.$message.warning("форма не валидна");
         }
       });
     },
